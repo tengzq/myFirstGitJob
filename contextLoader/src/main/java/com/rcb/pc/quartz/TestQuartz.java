9b65bde402201197ea8695ddd4e708e9018a0d7e
tzq <tzqhardworking@163.com>
2016-09-19 11:36:01 +0800

    收到

--- contextLoader/src/main/java/com/rcb/pc/quartz/TestQuartz.java
+++ contextLoader/src/main/java/com/rcb/pc/quartz/TestQuartz.java
@@ -1,9 +1,14 @@
 package com.rcb.pc.quartz;
 
 public class TestQuartz {
+	public void dsd () {
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("dsdsdsds");
 		System.out.println("dsdsddsdsd");
 		System.out.println("dsdsdsdsdsdsdsds");
+		
 	}
 }
